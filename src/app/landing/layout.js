7ef85532fed9e7110e77d05f4dbd1a30f0825cf5
laura <mbernallaura@gmail.com>
2024-09-18 12:41:04 -0500

    arreglar 3 intento despliegue

--- src/app/landing/layout.js
+++ src/app/landing/layout.js
@@ -2,11 +2,15 @@ import Navbar from "@/components/landing/Navbar";
 
 export default function LandingLayout({ children }) {
     return (
-        <html lang="en">
-            <body>
-                <Navbar/>
-                {children}
-            </body>
-        </html>
+        // <html lang="en">
+        //     <body>
+        //         <Navbar/>
+        //         {children}
+        //     </body>
+        // </html>
+        <div>
+            <Navbar/>
+            {children}
+        </div>
     );
 }
