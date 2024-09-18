0ebb4be925a4c0f854e9d5a072ba9a1d91adf2eb
laura <mbernallaura@gmail.com>
2024-09-18 12:26:51 -0500

    limpiando codigo

--- src/components/landing/Search.jsx
+++ src/components/landing/Search.jsx
@@ -3,14 +3,12 @@ import Image from "next/image";
 
 export const Search = () => {
     return (
-        // <div className="w-1/5 p-4 text-white bg-grayDark">
-            <div className="flex flex-col gap-4 mt-4 border-b-2 border-grayLight ">
-                <label>Search</label>
-                <div className="flex w-full px-4 justify-between items-center rounded-md bg-blackDark">
-                    <input type="text" className="w-full py-2 rounded-md bg-blackDark" placeholder="Keywords"/>
-                    <Image src={'/images/landing/iconSearch.png'} width={15} height={15}/>
-                </div>
+        <div className="flex flex-col gap-4 mt-4 border-b-2 border-grayLight ">
+            <label>Search</label>
+            <div className="flex w-full px-4 justify-between items-center rounded-md bg-blackDark">
+                <input type="text" className="w-full py-2 rounded-md bg-blackDark" placeholder="Keywords"/>
+                <Image src={'/images/landing/iconSearch.png'} width={15} height={15}/>
             </div>
-        // </div>
+        </div>
     )
 }
