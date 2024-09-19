06722678eb7b13d02ea68403cac8968a43cd9662
laura <mbernallaura@gmail.com>
2024-09-18 21:53:59 -0500

    traer info para el panel y acomodarlo en el espacio

--- src/app/landing/page.jsx
+++ src/app/landing/page.jsx
@@ -1,11 +1,15 @@
 'use client';
 import { Cards, Genres, Search } from "@/components/landing";
 import { getPopularMovies } from "@/lib/APICalls";
+import Image from "next/image";
 import { useEffect, useState } from "react";
 
 const LandingPage = () => {
     const urlImg = 'https://image.tmdb.org/t/p/w500';
+    const urlImgPanel = 'https://image.tmdb.org/t/p/w1280';
     const [popularMovies, setPopularMovies] = useState([]);
+    const firstMovie = popularMovies[0] || {};
+    const remainingMovies = popularMovies.slice(1);
 
 
     useEffect(() => {
@@ -23,8 +27,28 @@ const LandingPage = () => {
 
     return (
         <div className="flex flex-col h-full bg-grayLight">
-            <div className="h-96 mt-16 bg-red-300">
-                panel
+            <div className="h-96 mt-16 shadow-lg bg-red-300">
+                {/* <div className="relative h-full overflow-hidden shadow-xl">
+                    {firstMovie.backdrop_path && (
+                        <Image 
+                            src={`${urlImgPanel}${firstMovie.backdrop_path}`}
+                            layout="fill" 
+                            objectFit="cover"
+                            objectPosition="top left"
+                            alt="Login Image"
+                        />
+                    )}
+                </div> */}
+                <div 
+                    className="absolute top-0 left-0 w-full h-1/2 bg-cover bg-center"
+                    style={{ backgroundImage: `url(${urlImgPanel}${firstMovie.backdrop_path})` }}
+                >
+                    {/* Texto encima de la imagen */}
+                    <div className="absolute bottom-0 left-0 w-full p-10 text-white bg-gradient-to-t from-black to-transparent">
+                        <label className="text-4xl font-bold">{firstMovie.title}</label>
+                        <p className="text-lg">{firstMovie.overview}</p>
+                    </div>
+                </div>
             </div>
             <div className="flex ">
                 <div className="flex flex-col w-1/5 p-4 text-white bg-grayDark">
@@ -35,12 +59,12 @@ const LandingPage = () => {
                     <label className=" text-2xl text-white">Popular</label>
                     <div className="grid gap-4" style={{ gridTemplateColumns: "repeat(auto-fill, minmax(200px, 1fr))" }}>
                         {
-                            popularMovies.map((popularMovie)=>(
+                            remainingMovies.map((popularMovie)=>(
                                 <Cards 
                                     key={popularMovie.id} 
                                     title={popularMovie.title} 
                                     date={ popularMovie.release_date} 
-                                    img={`${urlImg}${popularMovie.backdrop_path}`}
+                                    img={`${urlImg}${popularMovie.poster_path}`}
                                 />
                             ))
                         }
