baabe05987b366a886592012c0a0b5d8c2751c1c
laura <mbernallaura@gmail.com>
2024-09-19 13:07:12 -0500

    Acomodar la maquetacion del registro

--- src/components/auth/SignUp.jsx
+++ src/components/auth/SignUp.jsx
@@ -1,14 +1,37 @@
-import Image from 'next/image';
+
 
 export const SignUp = () => {
     return (
         <div className="flex-grow content-center">
-            <button className="flex justify-center w-full py-4 rounded-lg text-black text-lg bg-yellow">
-                <div className="flex items-center content-center gap-2">
-                    <label>Register with your email</label>
-                    <Image className="h-fit" src="/images/login/iconEmail.png" width={15} height={15} ></Image>
-                </div>
-            </button>
+            <label className="flex justify-center font-bold text-4xl">Register</label>
+            <form className="my-14 text-black">
+                <input 
+                    type="text" 
+                    placeholder="Nombre"
+                    className="w-full py-4 px-6 rounded-lg" 
+                    // value={ email }
+                    name='name'
+                    // onChange={ onInputChange }
+                />
+                <input 
+                    type="text" 
+                    placeholder="Email"
+                    className="w-full py-4 px-6 mt-10 rounded-lg" 
+                    // value={ email }
+                    name='email'
+                    // onChange={ onInputChange }
+                />
+                <input 
+                    type="password" 
+                    placeholder="Password"
+                    className="w-full py-4 px-6 mt-10 rounded-lg"
+                    // value={ password } 
+                    name='password'
+                    // onChange={ onInputChange }
+                />
+                <button type="submit" className="w-full py-4 mt-20 text-black text-lg rounded-lg bg-yellow">Register</button>
+                
+            </form>
         </div>
     )
 }
